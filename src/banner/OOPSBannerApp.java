8d56c0949a88b889e0565571b5183eed2ae48bf1
jyothiradithya567 <jyothiradithya5672gmail.com>
2026-02-24 00:14:47 +0530

    UC2: Display OOPS in banner format

--- src/banner/OOPSBannerApp.java
+++ src/banner/OOPSBannerApp.java
@@ -1,13 +1,8 @@
-package banner;
+public static void main(String[] args) {
 
-public class OOPSBannerApp {
+    System.out.println(" ****   ****  ****  **** ");
+    System.out.println("*    * *    * *    * *    *");
+    System.out.println("*    * *    * *    * *    *");
+    System.out.println(" ****   ****  ****  **** ");
 
-    public static void main(String[] args) {
-
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println(" *   *   *   *   *   *   *     ");
-        System.out.println(" *   *   *   *   *   *   ***** ");
-        System.out.println(" *   *   *   *   *   *       * ");
-        System.out.println(" *****   *****   *****   ***** ");
-    }
 }
