5710ef0308d86a3a2556b9265df199f8fb36ddd6
jyothiradithya567 <jyothiradithya5672gmail.com>
2026-02-23 23:32:43 +0530

    UC1: Print OOPS Banner

---
+++ src/banner/OOPSBannerApp.java
@@ -0,0 +1,13 @@
+package banner;
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println(" *   *   *   *   *   *   *     ");
+        System.out.println(" *   *   *   *   *   *   ***** ");
+        System.out.println(" *   *   *   *   *   *       * ");
+        System.out.println(" *****   *****   *****   ***** ");
+    }
+}
