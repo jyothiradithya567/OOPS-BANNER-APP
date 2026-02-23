a964f75a5e7479b4695238960927c8741a3000d0
jyothiradithya567 <jyothiradithya5672gmail.com>
2026-02-24 01:20:32 +0530

    UC4: Corrected OOPS banner output

--- src/banner/OOPSBannerApp.java
+++ src/banner/OOPSBannerApp.java
@@ -1,8 +1,23 @@
-public static void main(String[] args) {
+package banner;
 
-    System.out.println(" ****   ****  ****  **** ");
-    System.out.println("*    * *    * *    * *    *");
-    System.out.println("*    * *    * *    * *    *");
-    System.out.println(" ****   ****  ****  **** ");
+class BannerPattern {
 
+    public static String[] getOOPSBanner() {
+        return new String[] {
+            "  ****    ****   *****    **** ",
+            " *    *  *    *  *    *  *     ",
+            " *    *  *    *  *****    ***  ",
+            " *    *  *    *  *           *",
+            "  ****    ****   *       ****  "
+        };
+    }
+}
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        for (String line : BannerPattern.getOOPSBanner()) {
+            System.out.println(line);
+        }
+    }
 }
